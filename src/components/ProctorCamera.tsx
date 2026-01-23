758c041af3976ac8afa7e3f372c3a77e1a17e9b8
nasvin2004 <161819057+nasvin2004@users.noreply.github.com>
2026-01-23 17:07:01 +0530

    Increase tab switch grace period to 3 seconds

--- src/components/ProctorCamera.tsx
+++ src/components/ProctorCamera.tsx
@@ -56,7 +56,7 @@ export default function ProctorCamera() {
   const isMobile =
   /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 
-  tabSwitchGraceUntilRef.current = Date.now() + 1000; // ⏱ 1 seconds grace
+  tabSwitchGraceUntilRef.current = Date.now() + 3000; // ⏱ 3 seconds grace
 
   /* ---------------- LOGGING ---------------- */
   const addLog = (type: LogItem["type"], message: string) => {
