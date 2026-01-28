1faa9e2b76be37312acc0575945a357b2ff217ff
nasvin2004 <161819057+nasvin2004@users.noreply.github.com>
2026-01-28 11:19:56 +0530

    Update import path for useBrowserProctoring hook

--- src/components/ProctorCamera.tsx
+++ src/components/ProctorCamera.tsx
@@ -5,7 +5,7 @@ import { classifyHeadPose } from "../ml/headClassifier";
 import { detectGaze } from "../ml/gazeDetector";
 import { decideMalpractice } from "../ml/headDecision";
 import { loadObjectModel, detectObjects } from "../ml/objectDetector";
-import { useBrowserProctoring } from "./useBrowserProctoring";
+import { useBrowserProctoring } from "../hooks/useBrowserProctoring";
 
 /* ---------------- TYPES ---------------- */
 type LogItem = {
