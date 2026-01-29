7bfd450289824aa691ddac4fc07af0e5becda419
nasvin2004 <161819057+nasvin2004@users.noreply.github.com>
2026-01-29 12:37:46 +0530

    Delete src/utils directory

--- src/utils/pwa.ts
+++
@@ -1,9 +0,0 @@
-export function isIOSStandalonePWA(): boolean {
-  return (
-    // iOS Safari "Add to Home Screen"
-    (window.navigator as any).standalone === true ||
-
-    // Modern PWA check
-    window.matchMedia("(display-mode: standalone)").matches
-  );
-}
